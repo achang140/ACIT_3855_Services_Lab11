2ea3f7aa77cf29df059f5bd89d3ac3fd553951b3
Amanda Chang <achang140@my.bcit.ca>
2024-04-01 10:21:32 -0700

    Commit

--- receiver/app.py
+++ receiver/app.py
@@ -151,7 +151,7 @@ def book_hotel_activity(body):
 
 app = connexion.FlaskApp(__name__, specification_dir="")
 # app.add_api("openapi.yaml", strict_validation=True, validate_responses=True) 
-app.add_api("openapi.yml", base_path="/receiver", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", base_path="/receiver", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     load(second_producer)
